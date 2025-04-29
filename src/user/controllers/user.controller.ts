737288203325f99b2ec7979c1d9ac4c8f93dcd1c
Eloypg <pardogurreaeloy@gmail.com>
2025-04-29 19:09:12 +0200

    feat: add get endpoints to user controller

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,17 +1,20 @@
 import {
   Body,
   Controller,
-  HttpStatus,
+  //HttpStatus,
   NotFoundException,
   Param,
   Post,
   Put,
   Delete,
+  Get,
 } from '@nestjs/common';
 import { UserService } from '../services/user.service';
 import { UserRequestDto } from '../dto/user-request.dto';
 import { UserResponseDto } from '../dto/user-response.dto';
 import { UserDtoAdapter } from '../adapters/user-dto.adapter';
+import { UserFilter } from '../filters/user.filter';
+import { UserModel } from '../models/user.model';
 
 @Controller('v1/user')
 /*@ApiSecurity('api_key') // ?????
@@ -30,6 +33,8 @@ import { UserDtoAdapter } from '../adapters/user-dto.adapter';
 })*/
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  // ADMIN CONTROLLER
 
   @Post()
   //@ApiOperation({ summary: 'Create a user' })
@@ -96,5 +101,72 @@ export class UserController {
     if (!deleted) {
       throw new NotFoundException(`User with id: ${userId} not found.`);
     }
+  }
+
+  // USER CONTROLLER
+
+  @Get('/:userId')
+  /*@ApiOperation({
+    summary: 'Get user',
+    description: 'Get one user by id',
+  })
+  @ApiOkResponse({
+    description: 'Return the selected user',
+    type: HttpStatus.OK,
+  })
+  @ApiNotFoundResponse({
+    description: 'User not found',
+    type: HttpStatus.NOT_FOUND,
+  })
+  @ApiParam({
+    name: 'userId',
+    type: String,
+    required: true,
+    //example: 'add fake uuid'
+  })*/
+  async findOne(@Param('userId') userId: string): Promise<UserResponseDto> {
+    const userResponse = await this.userService.findOne({
+      userId,
+    });
+
+    if (!userResponse) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+
+    return UserDtoAdapter.fromModelToResponseDto(userResponse);
+  }
+
+  @Get()
+  /*@ApiOperation({
+    summary: 'Get all users',
+  })
+  @ApiOkResponse({
+    description: 'Return all users',
+    type: HttpStatus.OK,
+  })
+  @ApiNotFoundResponse({
+    description: 'Users not found',
+    type: HttpStatus.NOT_FOUND,
+  })
+  @ApiParam({
+    name: 'filter',
+    type: UserFilter,
+    required: false,
+    example: {},
+  })*/
+  async find(@Param('filter') filter: UserFilter): Promise<UserResponseDto[]> {
+    const usersModels = await this.userService.findAll(filter);
+
+    if (!usersModels) {
+      throw new NotFoundException('Users not found');
+    }
+
+    const usersResponse = usersModels.map(
+      (userModel: UserModel): UserResponseDto => {
+        return UserDtoAdapter.fromModelToResponseDto(userModel);
+      },
+    );
+
+    return usersResponse;
   }
 }
